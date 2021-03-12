f63dc18b60555d1f872547c46c8b1d8ed1f9e7db
Alex Vezeaum <alex.vezeau@gmail.com>
2021-03-12 14:35:55 +0000

    backup

---
+++ publish.R
@@ -0,0 +1,12 @@
+library(rpubs)
+library(rsconnect)
+library(rmarkdown)
+
+options(echo = TRUE)
+
+rmarkdown::render("COVID_US.Rmd")
+rmarkdown::render("COVID_CANADA.Rmd")
+
+rsconnect::rpubsUpload(contentFile = "COVID_US.html", originalDoc = "", title = "COVID_US", id = "https://api.rpubs.com/api/v1/document/714182/273d2c03f0c34ed1ac27bfb951755121")
+rsconnect::rpubsUpload(contentFile = "COVID_CANADA.html", originalDoc = "", title = "COVID_CANADA", id = "https://api.rpubs.com/api/v1/document/714183/eda97654c2a744cd8a3dc0efe4ea61ce")
+
