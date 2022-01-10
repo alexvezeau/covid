f42a9738f8ff226107b16eaa663e84dd9bfa362e
Alex Vezeaum <alex.vezeau@gmail.com>
2022-01-10 15:29:44 +0000

    publish

--- publish.R
+++ publish.R
@@ -6,8 +6,8 @@ library(rmarkdown)
 options(echo = TRUE)
 
 rmarkdown::render("COVID_US.Rmd")
-rmarkdown::render("COVID_CANADA.Rmd")
-
 rsconnect::rpubsUpload(contentFile = "COVID_US.html", originalDoc = "", title = "COVID_US", id = "https://api.rpubs.com/api/v1/document/714182/273d2c03f0c34ed1ac27bfb951755121")
+
+rmarkdown::render("COVID_CANADA.Rmd")
 rsconnect::rpubsUpload(contentFile = "COVID_CANADA.html", originalDoc = "", title = "COVID_CANADA", id = "https://api.rpubs.com/api/v1/document/714183/eda97654c2a744cd8a3dc0efe4ea61ce")
 
